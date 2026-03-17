fcc0c541bc612663bb7c27046eba51c3f8fd7e5f
Iqra <iqraazam4831@gmail.com>
2026-03-17 22:51:37 +0500

    finally checked everything

--- greedybear/cronjobs/base.py
+++ greedybear/cronjobs/base.py
@@ -20,7 +20,6 @@ class Cronjob(metaclass=ABCMeta):
             self.success = True
         except Exception as e:
             self.log.exception(e)
-            raise    # <--- this line ensures failures propagate
+            raise # <--- this line ensures failures propagate
         finally:
-            self.log.info("Finished execution")
-           
+            self.log.info("Finished execution")           
